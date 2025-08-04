bf8fd456043d6c227b868e04ab1affadeccb5055
chinnawat sreesangjun <165617214+chinnawatsree@users.noreply.github.com>
2025-08-04 14:32:26 +0700

    Add files via upload

---
+++ home.php
@@ -0,0 +1,15 @@
+<!-- home.php -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+  <title>Home</title>
+  <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body class="p-6">
+  <h1 class="text-3xl font-semibold mb-4">🏠 หน้าหลัก</h1>
+  <p class="text-gray-700">ยินดีต้อนรับเข้าสู่หน้าแรก</p>
+</body>
+</html>
